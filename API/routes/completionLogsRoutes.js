d9ab6b2cc2e1257b2d68abc212563f8c9579ce43
Aidges Infotech <153446364+aidgesinfotech@users.noreply.github.com>
2025-11-20 23:04:32 +0530

    Delete API directory

--- API/routes/completionLogsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { auth } = require('../middlewares/auth');
-const completionLogsController = require('../controllers/completionLogsController');
-
-// List completion logs for a participant within an event
-router.get('/completion-logs/events/:eventId/participants/:participantId', auth, completionLogsController.listForParticipant);
-
-module.exports = router;
--- API/routes/scanRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const scanController = require('../controllers/scanController');
-const { staffAuth } = require('../middlewares/staffAuth');
-
-// Protected by staff login
-router.post('/mark-activity', staffAuth, scanController.markActivity);
-
-module.exports = router;
